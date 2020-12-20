2e1a73e311c2690ef1cfba2a8a5928ba7b343017
Donghyeonkim981214 <43703346+Donghyeonkim981214@users.noreply.github.com>
2020-12-20 21:08:19 +0900

    Modify index.html, views.py-index

--- my_to_do_app/views.py
+++ my_to_do_app/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+
 from .models import *
 
+"""
 def index(request):
     todos = Todo.objects.all()
     content = {'todos': todos}
     return render(request, 'my_to_do_app/index.html', content)
+"""
+class IndexView(generic.ListView):
+    template_name = 'my_to_do_app/index.html'
+    model = Todo
 
 def createTodo(request):
     user_input_str = request.POST['todoContent']
