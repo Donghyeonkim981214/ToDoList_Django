0c4f81009d20875048a7c3ead2b034711fde093c
Donghyeonkim981214 <43703346+Donghyeonkim981214@users.noreply.github.com>
2020-12-20 20:40:26 +0900

    Modify urls.py

--- my_to_do_app/urls.py
+++ my_to_do_app/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('createTodo/', views.createTodo, name ="createTodo"),
-    path('doneTodo/', views.doneTodo, name ='doneTodo')
+    #path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name = 'index'),
+
+    #path('createTodo/', views.createTodo, name="createTodo"),
+    path('createTodo/', views.CreateTodoView.as_view(), name="createTodo"),
+
+    #path('doneTodo/', views.doneTodo, name='doneTodo')
+    path('doneTodo/', views.DoneTodoView.as_view(), name='doneTodo')
 ]
