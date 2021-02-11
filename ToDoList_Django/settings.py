b8d2345134ec9d0505f3d7af9a81b8394a55be09
Donghyeonkim981214 <43703346+Donghyeonkim981214@users.noreply.github.com>
2021-02-11 23:54:13 +0900

    templates modify

--- ToDoList_Django/settings.py
+++ ToDoList_Django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,16 +45,6 @@ PROJECT_APPS =[
 ]
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
-""" INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'my_to_do_app'
-] """
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -69,7 +60,7 @@ ROOT_URLCONF = 'ToDoList_Django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
