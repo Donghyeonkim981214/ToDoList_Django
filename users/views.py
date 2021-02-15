1d151eed9282bf3b0d3bfe20509dd5f65fbb0c29
Donghyeonkim981214 <43703346+Donghyeonkim981214@users.noreply.github.com>
2021-02-16 02:15:25 +0900

    CBV-LoginView login Complite

--- users/views.py
+++ users/views.py
@@ -1,23 +1,13 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 
-from django.views.generic import FormView
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 
 from . import forms
 
-class login_view(FormView):
+class login_view(LoginView):
     template_name = "users/login.html"
-    form_class = forms.LoginForm
-    success_url = reverse_lazy("home:index")
-
-    def form_valid(self, form):
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-        user = authenticate(self.request, username=username, password=password)
-        if user is not None:
-            login(self.request, user)
-        return super().form_valid(form)
 
 def log_out(request):
     logout(request)
